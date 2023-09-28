76cd47ca435327c2cc752662559c15f0ddb9b5fa
Leandro Siow <leandroid@google.com>
2023-09-28 13:54:35 -0700

    Rename init scripts

--- setup_fuse_gcs.sh
+++ setup_fuse_gcs.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+
+echo "What is your GCS bucket name?"
+
+#Read input in the assigned variable
+read GCS_BUCKET_NAME
+
+
 ## 1 Install Fuse
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install fuse gcsfuse
+gcsfuse -v
 
+
+gcloud auth application-default login
+mkdir "$HOME/mount-folder"
+gcsfuse $GCS_BUCKET_NAME "$HOME/mount-folder"
